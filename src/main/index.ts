6acbeb6cb8439fcca07bb12aecc386f57b09b3c1
Rowan Liu <V176RRL+cpa@cathaypacific.com>
2025-11-28 18:38:46 +0800

    feat: update style

--- src/main/index.ts
+++ src/main/index.ts
@@ -9,6 +9,8 @@ function createWindow(): void {
   const mainWindow = new BrowserWindow({
     width: 900,
     height: 670,
+    minWidth: 460,
+    backgroundColor: '#050914',
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
